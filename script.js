d2461ff5184740a6adad8c4e71a5d9f5e5141ee4
Joshua Liew <liqshann@gmail.com>
2025-08-10 22:57:53 +0900

    feat: add 16 rows with js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const h = document.createElement("h1");
-h.textContent = "Hello, world!";
+const container = document.querySelector(".container");
 
-const div = document.querySelector("div");
-div.appendChild(h);
+const NUM_OF_ROWS = 16;
+for (let i = 0; i < NUM_OF_ROWS; i++) {
+    const div = document.createElement("div");
+    const cls = ["row", `row-${i}`,];
+    div.classList.add(...cls);
+    div.style["border"] = "0.25rem solid red";
+    div.style["height"] = "25px";
+    container.appendChild(div);
+}
