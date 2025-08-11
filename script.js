6db7be7120d2b98e0ec19ed15271f11701e6b89a
Joshua Liew <liqshann@gmail.com>
2025-08-11 13:26:36 +0900

    refactor: dynamically add rows and cols

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 const container = document.querySelector(".container");
+container.style["width"] = "600px";
+container.style["height"] = "600px";
 container.style["display"] = "flex";
 container.style["flexDirection"] = "column";
 
-const NUM_OF_ROWS = 16;
-for (let i = 0; i < NUM_OF_ROWS; i++) {
-    const div = document.createElement("div");
-    const cls = ["row", `row-${i}`,];
-    div.classList.add(...cls);
-    div.style["border"] = "0.25rem solid red";
-    div.style["height"] = "25px";
-    div.style["display"] = "flex";
-    div.style["flexDirection"] = "row";
-    div.style["justifyContent"] = "space-between";
-    container.appendChild(div);
+function createRows(numRows) {
+    const NUM_OF_ROWS = numRows;
+    for (let i = 0; i < NUM_OF_ROWS; i++) {
+        const div = document.createElement("div");
+        const cls = ["row", `row-${i}`,];
+        div.classList.add(...cls);
+        div.style["border"] = "0.25rem solid red";
+        div.style["display"] = "flex";
+        div.style["flexDirection"] = "row";
+        div.style["height"] = "100%";
+        div.style["justifyContent"] = "space-between";
+        container.appendChild(div);
+    }
 }
 
-const NUM_OF_COLS = 16;
-const rowList = document.querySelectorAll(".row");
-rowList.forEach((row) => {
-    for (let i = 0; i < NUM_OF_COLS; i++) {
-        const div = document.createElement("div");
-        const cls = ["col", `col-${i}`, "grid-item"];
-        div.classList.add(...cls);
-        div.style["border"] = "0.25rem solid blue";
-        div.style["width"] = "100%";
-        row.appendChild(div);
-    }
-});
+function createCols(numCols) {
+    const NUM_OF_COLS = numCols;
+    const rowList = document.querySelectorAll(".row");
+    rowList.forEach((row) => {
+        for (let i = 0; i < NUM_OF_COLS; i++) {
+            const div = document.createElement("div");
+            const cls = ["col", `col-${i}`, "grid-item"];
+            div.classList.add(...cls);
+            div.style["border"] = "0.25rem solid blue";
+            div.style["width"] = "100%";
+            row.appendChild(div);
+        }
+    });
+}
 
 const gridItemList = document.querySelectorAll(".grid-item");
 gridItemList.forEach((gridItem) => {
@@ -38,13 +44,12 @@ gridItemList.forEach((gridItem) => {
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("You've submitted the form!");
+    // TODO: add form logic
     const formData = new FormData(form);
-    formData.keys().forEach((key) => {
-        console.log(key);
-    });
-    const numRows = formData.get("input-row");
-    const numCols = formData.get("input-col");
-    console.log(numRows);
-    console.log(numCols);
+    const numRows = formData.get("num-rows");
+    const numCols = formData.get("num-cols");
+    // TODO: add data validation
+
+    createRows(numRows);
+    createCols(numCols);
 });
